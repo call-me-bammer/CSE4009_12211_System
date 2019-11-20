6d9525d7cdc671cfce43dc8dd56411948cbb7433
2018009143 <jhcse5189@gmail.com>
2019-11-20 15:29:08 +0900

    add. Lab4 dir.

---
+++ ch13_linker/symbol_is_weak.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int x;
+
+int main(int argc, char* argv[]) {
+
+    printf("%d\n", x);
+    return 0;
+}
---
+++ exception/divbyzero.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("n is %d\n", n);
+
+    printf("10 div by what: %d\n", 10 / n);
+    return 0;
+}
